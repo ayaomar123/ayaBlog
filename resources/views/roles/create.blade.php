192ae7db3edede3a9215e0d7afce43b8b7166fbd
ayaomar123 <yotayot29@gmail.com>
2021-01-17 14:09:23 +0200

    ux

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -17,8 +17,6 @@
     </div>
 </div>
 
-
-
     @if (count($errors) > 0)
         <div class="alert alert-danger">
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -31,6 +29,57 @@
     @endif
 
     {!! Form::open(array('route' => 'roles.store','method'=>'POST')) !!}
+    <div class="card mt-3" style="margin-left:50px;margin-right:50px;text-align:left;padding-left:25px ">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="form-group mt-2">
+                    <strong>Name:</strong>
+                    {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                </div>
+            </div>
+        </div>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+              <th scope="col">Permission</th>
+              <th scope="col">View</th>
+              <th scope="col">Add</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <th scope="row">Category</th>
+              <td><input class="name" name="permission[]" type="checkbox" value="9"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="10"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="11"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="12"></td>
+            </tr>
+            <tr>
+              <th scope="row">Product</th>
+              <td><input class="name" name="permission[]" type="checkbox" value="5"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="6"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="7"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="8"></td>
+            </tr>
+            <tr>
+              <th scope="row">Article</th>
+              <td><input class="name" name="permission[]" type="checkbox" value="13"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="14"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="15"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="16"></td>
+            </tr>
+        <tr>
+          <th scope="row">Role</th>
+          <td><input class="name" name="permission[]" type="checkbox" value="1"></td>
+          <td><input class="name" name="permission[]" type="checkbox" value="2"></td>
+          <td><input class="name" name="permission[]" type="checkbox" value="3"></td>
+          <td><input class="name" name="permission[]" type="checkbox" value="4"></td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
     <div class="card mt-3" style="margin-left:50px;margin-right:50px;text-align:left;padding-left:25px ">
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-6">
--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -32,24 +32,55 @@
 
     {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
     <div class="card mt-3" style="margin-left:50px;margin-right:50px;text-align:left;padding-left:25px ">
-    <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6">
             <div class="form-group mt-3">
                 <h4>Name:</h4>
                 {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group mt-">
-                <h4>Permission:</h4>
-                <br/>
-                @foreach($permission as $value)
-                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-                        {{ $value->name }}</label>
-                    <br/>
-                @endforeach
-            </div>
-        </div>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                  <th scope="col">Permission</th>
+                  <th scope="col">View</th>
+                  <th scope="col">Add</th>
+                  <th scope="col">Edit</th>
+                  <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <th scope="row">Category</th>
+                  <td><input class="name" name="permission[]" type="checkbox" value="9"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="10"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="11"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="12"></td>
+                </tr>
+                <tr>
+                  <th scope="row">Product</th>
+                  <td><input class="name" name="permission[]" type="checkbox" value="5"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="6"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="7"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="8"></td>
+                </tr>
+                <tr>
+                  <th scope="row">Article</th>
+                  <td><input class="name" name="permission[]" type="checkbox" value="13"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="14"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="15"></td>
+                  <td><input class="name" name="permission[]" type="checkbox" value="16"></td>
+                </tr>
+            <tr>
+              <th scope="row">Role</th>
+              <td><input class="name" name="permission[]" type="checkbox" value="1"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="2"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="3"></td>
+              <td><input class="name" name="permission[]" type="checkbox" value="4"></td>
+            </tr>
+            </tbody>
+        </table>
+
+
         <div class="col-xs-12 col-sm-12 col-md-6 text-center mb-5">
             <button style="float: left" type="submit" class="btn btn-primary">Submit</button>
         </div>
