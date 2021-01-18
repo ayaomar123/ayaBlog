3a0da2684684201d6a16501ebfa7bf000b857ebd
ayaomar123 <yotayot29@gmail.com>
2021-01-18 11:05:49 +0200

    y

---
+++ database/migrations/2020_12_23_083911_create_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('face-URL');
+            $table->string('twitter-URL');
+            $table->string('youtube-URL');
+            $table->string('instagram-URL');
+            $table->string('snapchat-URL');
+            $table->string('linkedin-URL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
